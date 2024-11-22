875d57c3e3a97f87c549bf0b4ec2c3d023fb3e8f
exerussus <solovyov.production@gmail.com>
2024-11-22 11:08:50 +0300

    Auto updated to version 1.0.6

--- Core/GameEntityStarter.cs
+++ Core/GameEntityStarter.cs
@@ -1,7 +1,7 @@
 using System.Linq;
-using Exerussus._1EasyEcs.Scripts.Core;
 using Exerussus._1EasyEcs.Scripts.Custom;
 using Exerussus._1Extensions.SignalSystem;
+using Exerussus._1Extensions.SmallFeatures;
 using Leopotam.EcsLite;
 using UnityEngine;
 #if UNITY_EDITOR
